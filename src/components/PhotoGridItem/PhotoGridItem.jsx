6bd41dfdb2db24600f6e48e6086453df65072177
Andres Ferrada <an.ferrada@gmail.com>
2026-02-24 23:05:04 -0300

    fix: ellipse only in the last li type tag

--- src/components/PhotoGridItem/PhotoGridItem.jsx
+++ src/components/PhotoGridItem/PhotoGridItem.jsx
@@ -62,9 +62,12 @@ const Tag = styled.li`
   font-size: 0.875rem;
   font-weight: 475;
   color: var(--color-gray-800);
-  overflow: hidden;
-  text-overflow: ellipsis;
   white-space: nowrap;
+
+  &:last-of-type {
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
 `
 
 export default PhotoGridItem
